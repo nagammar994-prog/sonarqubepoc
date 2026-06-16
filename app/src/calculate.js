fd3f4f57bb14c0bd577dc6d413c8c3f93fca5f8b
nagammar994-prog <nagammar994@gmail.com>
2026-06-16 16:58:23 +0530

    Add calculate function to sum an array of numbers

---
+++ app/src/calculate.js
@@ -0,0 +1,16 @@
+function calculate(data) {
+
+    let total = 0;
+
+    for(let i=0;i<data.length;i++){
+        total += data[i];
+    }
+
+    for(let i=0;i<data.length;i++){
+        total += data[i];
+    }
+
+    return total;
+}
+
+module.exports = calculate;
